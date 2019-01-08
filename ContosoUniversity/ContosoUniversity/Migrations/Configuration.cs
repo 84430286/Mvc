a1cab9fd948ccb0c367d9ce7d25f9ac2b22efedb
84430286 <84430286@qq.com>
2019-01-08 17:03:18 +0800

    1
    1

--- ContosoUniversity/ContosoUniversity/Migrations/Configuration.cs
+++ ContosoUniversity/ContosoUniversity/Migrations/Configuration.cs
@@ -16,10 +16,7 @@ namespace ContosoUniversity.Migrations
 
         protected override void Seed(ContosoUniversity.DAL.SchoolContext context)
         {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data.
+            
 
             var students = new List<Student>
             {
